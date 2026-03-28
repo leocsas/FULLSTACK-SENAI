f585487953abc2efa59289a9f5bf83e5e4942aaa
leocsas <leocristian2014@gmail.com>
2026-03-28 15:40:01 -0300

    Incrementação: adicionando funcionalidade de adição ao carrinho

--- ARENA-GAMER/js/script.js
+++ ARENA-GAMER/js/script.js
@@ -9,3 +9,27 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+const contador = document.getElementsByClassName('contador-carrinho');
+const adicionar = document.getElementsByClassName('botao-carrinho');
+const mensagem = document.getElementsByClassName('mensagem-sucesso');
+
+let count = 0;
+
+const updateContador = () => {
+    contador[0].innerHTML = count;
+};
+
+for (let i = 0; i < adicionar.length; i++) {
+    adicionar[i].addEventListener('click', () => {
+    count += 1;
+    updateContador();
+
+    mensagem[0].style.display = 'block';
+
+    setTimeout(() => {
+        mensagem[0].style.display = 'none';
+    }, 1500);
+});
+}
+
